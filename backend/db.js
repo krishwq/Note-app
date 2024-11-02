572cea30ad398a5ad5add487966b094de41f008f
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2024-11-02 20:49:35 +0530

    change

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const mongoURI="mongodb+srv://23ee01025:KrishnenduBir@cluster0.f4nm2.mongodb.net/notes?retryWrites=true&w=majority&appName=Cluster0"
+
 const ConnectToMongo=()=>{
     mongoose.connect(mongoURI,).then(()=>console.log("connected"))
 }
