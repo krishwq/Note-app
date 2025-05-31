4eef59c7797fb166493f1683ab618e72708d7492
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2025-05-31 20:47:29 +0530

    change

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -518,7 +518,7 @@ function Login() {
           </div>
           <ReCAPTCHA
             ref={(r) => setlogCaptchaRef(r)}
-            sitekey="6LdYToYqAAAAAIhpZlrFGJQubFWiGmaK1beMC1fF"
+            sitekey="6LfpVlErAAAAAP3Oq377_p1jZ5LiNcVFzE6tr8SP"
             onChange={onChangelogin}
           />
           <button
